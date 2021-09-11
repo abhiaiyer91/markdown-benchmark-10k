f287f8e852aa489964209ad676c61aca57fca5d5
Dustin Schau <dustinschau@gmail.com>
2021-09-10 19:31:33 -0700

    chore: add prev/next

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,20 +1,26 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 
-const Index = ({ data }) => (
-  <main>
-    <h1>Hello World!</h1>
-    <ul>
-      {
-        data.allFakeMarkdown.nodes.map(node => (
-          <li key={node.slug}>
-            <Link to={node.slug}>{node.title}</Link>
-          </li>
-        ))
-      }
-    </ul>
-  </main>
-)
+const Index = ({ data, pageContext: { currentPage } }) => {
+  const prev = currentPage === 1 ? null : <Link to={`/blog/${currentPage - 1}/`}>Previous</Link>
+  const next = <Link to={`/blog/${currentPage + 1}/`}>Next</Link>
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <ul>
+        {
+          data.allFakeMarkdown.nodes.map(node => (
+            <li key={node.slug}>
+              <Link to={node.slug}>{node.title}</Link>
+            </li>
+          ))
+        }
+      </ul>
+      {prev}
+      {next}
+    </main>
+  )
+}
 
 export const indexQuery = graphql`
   query BlogPage($skip: Int!, $limit: Int!) {
